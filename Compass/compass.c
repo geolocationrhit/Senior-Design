8d356bc71df13fdc4307f0cfd35d48fdea164988
Jesse Brannon <brannojs@rose-hulman.edu>
2012-10-13 05:42:52 -0400

    Added compass code and example "main" file

---
+++ Compass/compass.c
@@ -0,0 +1,39 @@
+#include "i2c-dev.h"
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+
+//This is the address of the Compass on the I2C Bus
+#define ADDRESS 0x21
+
+int init_I2C(void){
+        int fd;
+        fd = open( "/dev/i2c-3", O_RDWR);
+        if( ioctl(fd, I2C_SLAVE, ADDRESS) < 0)
+        {
+                fprintf(stderr, "Failed to set slave address: %m\n");
+                return 2;
+        }
+        return fd;
+}
+
+//Setup the compass and set it to continous output mode
+void init_compass(int fd){
+        char buf[10] = {0};
+        buf[0] = 'G';
+        buf[1] = 0x74;
+        buf[2] = 0b01100010;
+        if(write(fd,buf,3)!=3)
+    			fprintf(stderr, "Failed to set to continous mode: %m\n");
+}
+
+float getHeading(int fd){
+        char data[10] = {0};
+        if(read(fd,data,2)!=2)
+                fprintf(stderr,"Failed to read compass: %m\n");
+        float result = ((data[0] << 8) + data[1])/10.0;
+        return result;
+}
---
+++ Compass/main.c
@@ -0,0 +1,18 @@
+#include "i2c-dev.h"
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <poll.h>
+#include "compass.c"
+
+int main(int argc, char *argv){
+        int fd = init_I2C();
+        init_compass(fd);
+        while(1){
+                usleep(200000);
+                float heading = getHeading(fd);
+                printf("Heading:%3.1f\n",heading);
+        }
+}
