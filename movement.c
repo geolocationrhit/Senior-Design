24754ad3d56ff803dfa2a3011d0f221e26996bd6
The Bone <root@beaglebone.(none)>
2012-10-25 16:43:15 +0000

    more testing

--- movement.c
+++ movement.c
@@ -8,13 +8,15 @@
 #include "i2c-dev.h"
 #include "Compass/compass.h" // booooooo
 #include <stdio.h>
+#include <signal.h>
 
-#define LEFT_FWD_GPIO 71
-#define LEFT_BCK_GPIO 73
+#define LEFT_BCK_GPIO 71
+#define LEFT_FWD_GPIO 73
 #define RIGHT_BCK_GPIO 75
 #define RIGHT_FWD_GPIO 77
 
 int fd;
+int keepGoing = 1;
 
 enum motorOps {
 	forward,
@@ -55,9 +57,11 @@ int turn(float heading){
 
 	const int tolerance = 1;
 	printf("deltaHeading: %f, tolerance: %d\n", deltaHeading, tolerance);
-	while(!((deltaHeading < tolerance) || (deltaHeading - 360 > -tolerance))){
+	while(abs(currentHeading - heading) > tolerance && currentHeading heading - 360
+/*	while(!((deltaHeading < tolerance) || (deltaHeading - 360 > -tolerance)) && keepGoing){
 		//printf("In the if! We should be turning....\n");
 		if(deltaHeading > 0){
+			printf("deltaHeading positive: %f, currentHeading: %f\n", deltaHeading, currentHeading);
 			if(deltaHeading < 180){ //turn left
 				gpio_set_value(LEFT_BCK_GPIO, 1);
 				gpio_set_value(LEFT_FWD_GPIO, 0);
@@ -70,6 +74,7 @@ int turn(float heading){
 				gpio_set_value(RIGHT_FWD_GPIO, 0);
 			}
 		} else {
+			printf("deltaHeading negative: %f, currentHeading: %f\n", deltaHeading, currentHeading);
 			if(deltaHeading < 180){ //turn right
 				gpio_set_value(LEFT_FWD_GPIO, 1);
 				gpio_set_value(LEFT_BCK_GPIO, 0);
@@ -83,8 +88,10 @@ int turn(float heading){
 			}
 		} 
 		//usleep(100);
+		currentHeading = getHeading(fd);
 		deltaHeading = currentHeading - heading;
 	}
+*/
 	gpio_set_value(LEFT_FWD_GPIO, 0);
 	gpio_set_value(LEFT_BCK_GPIO, 0);
 	gpio_set_value(RIGHT_FWD_GPIO, 0);
@@ -99,13 +106,25 @@ int move(gpsPoint gpsCoord){
 	int originalLineM, originalLineB;
 }	
 
+void signal_handler(int sig)
+{
+        printf( "Program Exited by User\n" );
+	gpio_set_value(LEFT_FWD_GPIO, 0);
+	gpio_set_value(LEFT_BCK_GPIO, 0);
+	gpio_set_value(RIGHT_FWD_GPIO, 0);
+	gpio_set_value(RIGHT_BCK_GPIO, 0);
+	keepGoing = 0;
+}
+
 void main(int * argv){
+	signal(SIGINT, signal_handler);
+
 	fd = init_I2C();
 	init_compass(fd);
 	gpioInit();
 	//testTurn();
-	//turn(45.0);
-	testTurn();
+	turn(45.0);
+	//testTurn();
 }
 
 
