bfcad3880729ac3718521ca20055cac6940c00c7
Beagle <hansenrl@rose-hulman.edu>
2012-10-26 20:26:19 -0400

    made ip and port numbers configurable

--- networkPython/client.py
+++ networkPython/client.py
@@ -1,13 +1,20 @@
 #!/usr/bin/python
 
 import socket
+import sys
 
-TCP_IP = '127.0.0.1'
-TCP_PORT = 5005
+if len(sys.argv) == 3:
+	TCP_IP = sys.argv[1]
+	TCP_PORT = int(sys.argv[2])
+else:
+	TCP_IP = "127.0.0.1"
+	TCP_PORT = 5006
+
 BUFFER_SIZE = 1024
 MESSAGE = "Hello, World!\n"
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print "Connecting to " + TCP_IP + " on port " + str(TCP_PORT)
 s.connect((TCP_IP, TCP_PORT))
 s.send(MESSAGE)
 data = s.recv(BUFFER_SIZE)
--- networkPython/server.py
+++ networkPython/server.py
@@ -1,31 +1,43 @@
 import SocketServer
+import sys
 
 class MyTCPHandler(SocketServer.BaseRequestHandler):
-    """
-    The RequestHandler class for our server.
+	"""
+	The RequestHandler class for our server.
 
-    It is instantiated once per connection to the server, and must
-    override the handle() method to implement communication to the
-    client.
-    """
+	It is instantiated once per connection to the server, and must
+	override the handle() method to implement communication to the
+	client.
+	"""
 
-    def handle(self):
-        # self.request is the TCP socket connected to the client
-        self.data = self.request.recv(1024).strip()
-        print "{} wrote:".format(self.client_address[0])
-        print self.data
-        # just send back the same data, but upper-cased
-        self.request.sendall("Hello\nThis is a test")
+	def handle(self):
+		# self.request is the TCP socket connected to the client
+		self.data = self.request.recv(1024).strip()
+		print "{} wrote:".format(self.client_address[0])
+		print self.data
+		# just send back the same data, but upper-cased
+		self.request.sendall("Hello\nThis is a test")
 
 if __name__ == "__main__":
-    HOST, PORT = "localhost", 5005
+	if len(sys.argv) == 3:
+		#print sys.argv
+		HOST = sys.argv[1]
+		PORT = int(sys.argv[2])
+	else:
+		HOST = "localhost"
+		PORT = 5006
 
-    # Create the server, binding to localhost on port 9999
-    server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
+	# Create the server, binding to localhost on port 9999
+	print "Setting up a server at " + HOST + " on port " + str(PORT)
+	server = SocketServer.TCPServer((HOST, PORT), MyTCPHandler)
 
-    # Activate the server; this will keep running until you
-    # interrupt the program with Ctrl-C
-    server.serve_forever()
+	# Activate the server; this will keep running until you
+	# interrupt the program with Ctrl-C
+	try:
+		server.serve_forever(0.5)
+	except KeyboardInterrupt:
+		print "\nShutting down the server"
+		server.shutdown()
 
 """
 #!/usr/bin/python
