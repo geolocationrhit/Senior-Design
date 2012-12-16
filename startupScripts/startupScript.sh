987fc84c754586eacef0585696a26d3354d3df75
Demo User <ubuntu@arm.(none)>
2012-12-15 18:07:34 -0600

    modified startup script to run the heartbeat python script

--- startupScripts/startupScript.sh
+++ startupScripts/startupScript.sh
@@ -2,6 +2,8 @@
 
 ifconfig eth0 up
 ifconfig eth0 192.168.10.1
+sleep 10
 python /home/ubuntu/Senior-Design/MATLABcode/server.py `ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'` 5005 &
+python /home/ubuntu/Senior-Design/MATLABcode/registerWithCP.py 192.168.2.50 5007 `ifconfig wlan0 | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
 
 exit 0
